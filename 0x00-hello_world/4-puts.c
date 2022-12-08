b4dff091b046b18f2619589bce2d8e37688ec0f6
RuthLesego <motseilesego@gmail.com>
2022-12-08 13:19:06 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - main block
- * return: 0
+ * return: o
  */
 int main(void)
 {
-	put("/"programming is like building a multilingual pizzle");
+	put("\"Programming is like building a multilingual puzzle");
 	return (0);
 }	
