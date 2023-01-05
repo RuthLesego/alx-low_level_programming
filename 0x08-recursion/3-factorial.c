5124e6a52fdcf1f06b29e408fa9556d6e85ea2b7
RuthLesego <motseilesego@gmail.com>
2023-01-05 04:07:15 -0800

    task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,17 +2,16 @@
 
 /**
  * factorial - Calculate the factorial of a number
- * @n: the number to calculate the factorial
+ * @n: integer
  *
- * Return: integer value
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
-
-	if (n <= 1)
-		return (-1);
-
+	if (n == 0)
+		return (1);
 	return (n * factorial(n - 1));
 }
