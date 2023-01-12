659587fd0ac331816fe8aa61301f81fb5c21c4a8
RuthLesego <motseilesego@gmail.com>
2023-01-12 11:58:29 -0800

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,14 +6,11 @@
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: Always 0 (Success)
+ * Return: Always zero
  */
-
 int main(int argc, char *argv[])
 {
-(void) argv; /*Ignore argv*/
+	(void) argv;
 	printf("%d\n", argc - 1);
-
 	return (0);
-
 }
