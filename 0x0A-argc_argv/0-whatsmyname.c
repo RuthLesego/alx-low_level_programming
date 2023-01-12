7cb5489197bfd09192f4f1fef610712d707df106
RuthLesego <motseilesego@gmail.com>
2023-01-12 06:35:04 -0800

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,14 +3,16 @@
 
 /**
  * main - Prints the name of the program
- * @argc: Number of command line arguments
- * @argv: Array name
- * Return: 0
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (success)
  */
 
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[argc * 0]);
+	(void) argc;
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
