a8e7369afe60f2d1bebd5f78c2ca0557652c90af
RuthLesego <motseilesego@gmail.com>
2023-01-14 15:47:08 -0800

    task 2

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+char*_stencat(char *dest, char *src, int n)
+{
+	return strncat(dest,nsrc, n);
+}
